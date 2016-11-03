e8fec0b9e8e002a804a0235d40a14d6a59498049
Karl Odie <karlodie@gmail.com>
2016-11-03 13:37:52 +0000

    Initialise initial core.  A REST interface for users.

---
+++ urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url, include
+from rest_framework import routers
+
+from .serializers.user import UserViewSet
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls'))
+]
