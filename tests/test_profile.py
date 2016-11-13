dd27352ea48816a7116a1005c826637f29d9d543
Karl Odie <karlodie@gmail.com>
2016-11-13 19:49:45 +0000

    Raise Profile model coverage to 100%

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -17,6 +17,15 @@ def test_profile_creation():
     # Check the default profile is set.
     assert profile.theme == '1'
     assert profile.theme_name() == 'Cerulean'
+    # Change the profile to Flatly.
+    profile.theme = '5'
+    assert profile.theme_name() == 'Flatly'
+    # Now change to an invalid profile.
+    profile.theme = '56'
+    assert profile.theme_name() is None
+    # Save something on the user.
+    user.first_name = 'Fred'
+    user.save()
     # Now check when the user is deleted, the profile goes as well.
     user.delete()
     with pytest.raises(Profile.DoesNotExist):
