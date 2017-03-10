c8b0d88ae627fd00c400d51b39a5f5b756417bb9
Karl Odie <karlodie@gmail.com>
2017-03-10 15:20:49 +0000

    Add a test to check the imports

---
+++ tests/test_import.py
@@ -0,0 +1,17 @@
+import sys
+import os
+
+
+def test_imports():
+    print(sys.path)
+    print(os.path.abspath('.'))
+    import nimble.migrations.m_0001_initial
+    import nimble.migrations.m_0002_profile_theme
+    import nimble.migrations.m_0003_auto_20161127_0953
+    import nimble.migrations.m_0004_auto_20161218_1303
+    import nimble.migrations.m_0005_story_description
+    assert dir(nimble.migrations.m_0001_initial)
+    assert dir(nimble.migrations.m_0002_profile_theme)
+    assert dir(nimble.migrations.m_0003_auto_20161127_0953)
+    assert dir(nimble.migrations.m_0004_auto_20161218_1303)
+    assert dir(nimble.migrations.m_0005_story_description)
